db81b260d2c4e9828617f00be4df2a4c285fbb48
Vladimir Rachkin <vova@kpnn.ru>
2023-09-12 16:21:15 +0300

    Create parser template

---
+++ server/src/main/kotlin/org/tod87et/Parser.kt
@@ -0,0 +1,20 @@
+package org.tod87et
+class Parser private constructor(formula: String) {
+
+    enum class Binop {
+        PLUS, MINUS, MULTIPLY, DIVIDE
+    }
+
+    class EvalException(message: String) : Exception(message)
+
+    companion object {
+
+        // Split by first number, sign or bracket
+        fun lex (s: String) : Pair<String, String> {
+            TODO()
+        }
+        fun eval(formula: String): Float {
+            TODO()
+        }
+    }
+}
---
+++ server/src/test/kotlin/org/tod87et/ParserTest.kt
@@ -0,0 +1,26 @@
+package org.tod87et
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.tod87et.Parser
+
+class ParserTest {
+
+    @Test
+    fun testLex() {
+        assertEquals(Parser.lex("42"), Pair("42", ""))
+
+        assertEquals(Parser.lex("42.11"), Pair("42.11", ""))
+
+        assertEquals(Parser.lex("42 ("), Pair("42", "("))
+
+        assertEquals(Parser.lex("+98 + 9"), Pair("+", "98 + 9"))
+
+        assertEquals(Parser.lex("+     98 + 9"), Pair("+", "98 + 9"))
+    }
+
+    @Test
+    fun testEval() {
+        assertEquals(Parser.eval("42"), 42f)
+    }
+}
