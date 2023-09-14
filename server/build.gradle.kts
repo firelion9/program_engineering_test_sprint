d314017b1481f721945fb91aa9905d16a502edcd
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-14 14:44:11 +0300

    Добавил зависимость от ktor-server-core и парсинг аргументов сервера

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -4,10 +4,14 @@ plugins {
 }
 
 group = "org.tod87et"
+val ktorVersion = extra["ktor.version"] as String
+val kotlinxCli = extra["kotlinx-cli.version"] as String
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:$kotlinxCli")
 }
 
 tasks.test {
