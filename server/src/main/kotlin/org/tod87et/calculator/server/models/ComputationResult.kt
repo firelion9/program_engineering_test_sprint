bdbbfab6b38faa888b26074147d83379a98be7be
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-15 01:52:43 +0300

    Поменял имена на такие, как в документе

--- server/src/main/kotlin/org/tod87et/calculator/server/models/ComputationResult.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/models/ComputationResult.kt
@@ -3,5 +3,6 @@ package org.tod87et.calculator.server.models
 import kotlinx.serialization.Serializable
 import kotlinx.datetime.Instant
 
+// This class will be used for communication between server and client
 @Serializable
-data class ComputationResult(val id: Int, val formula: String, val result: Double, val date: Instant)
+data class ComputationResult(val id: String, val expression: String, val result: Double, val timestamp: Instant)
