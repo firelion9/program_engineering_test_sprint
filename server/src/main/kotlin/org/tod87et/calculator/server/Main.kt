62778833e1d19dcda80b63d944abee923a3a8f5d
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-15 00:09:48 +0300

    Изменил Main(убрал все из args, информация о базе данных из переменных окружения), добавил конфигурацию сервера в файле, logback

--- server/src/main/kotlin/org/tod87et/calculator/server/Main.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/Main.kt
@@ -1,36 +1,10 @@
 package org.tod87et.calculator.server
 
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.default
-import kotlinx.cli.required
-
-const val DEFAULT_PORT = 8080
-const val DEFAULT_ADDRESS = "127.0.0.1"
-
 fun main(args: Array<String>) {
-    val parser = ArgParser("Calculator Server")
-    val ipAddress by parser.option(ArgType.String,
-        fullName = "address",
-        description = "ip address for server"
-    ).default(DEFAULT_ADDRESS)
-    val port by parser.option(ArgType.Int,
-        fullName = "port",
-        description = "port for server"
-    ).default(DEFAULT_PORT)
-    val databaseAddress by parser.option(ArgType.String,
-        fullName = "db_url",
-        description = "Database url to connect to"
-    ).required()
-    val databaseDriver by parser.option(ArgType.String,
-        fullName = "db_driver",
-        description = "Database driver for correct connection"
-    ).default("org.postgresql.Driver")
-    val databaseUser by parser.option(ArgType.String,
-        fullName = "db_user",
-        description = "Database user for login").default("")
-    val databasePassword by parser.option(ArgType.String,
-        fullName = "db_password",
-        description = "Database password for login").default("")
-    parser.parse(args)
+    val databaseAddress = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost"
+    val databaseDriver = System.getenv("DB_DRIVER") ?: "org.postgresql.Driver"
+    val databaseUser = System.getenv("DB_USER") ?: ""
+    val databasePassword = System.getenv("DB_PASSWORD") ?: ""
+    io.ktor.server.netty.EngineMain.main(args)
 }
+
