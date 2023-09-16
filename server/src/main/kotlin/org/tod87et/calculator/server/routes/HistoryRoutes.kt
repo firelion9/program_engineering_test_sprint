95ca526a5676303ec917ddadb3a74e774a2f73db
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-16 18:00:12 +0300

    добавил query parameters и убрал ненужную либу

--- server/src/main/kotlin/org/tod87et/calculator/server/routes/HistoryRoutes.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/routes/HistoryRoutes.kt
@@ -4,12 +4,15 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.runBlocking
 import org.tod87et.calculator.server.database
 
 fun Route.historyRouting() {
     route("/history") {
         route("/list") {
-            get {
+            get() {
+                val limit = call.request.queryParameters["limit"]?.toIntOrNull()
+                val offset = call.request.queryParameters["offset"]?.toIntOrNull()
                 //TODO
             }
         }
