5570eba7d50ffcdd4c06e7a4f92d53b23e825cce
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-16 19:03:34 +0300

    Поправил дефолтный DB_URL

--- server/src/main/kotlin/org/tod87et/calculator/server/Main.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/Main.kt
@@ -4,7 +4,7 @@ import org.tod87et.calculator.server.database.FormulasDb
 
 public lateinit var database: FormulasDb
 fun main(args: Array<String>) {
-    val databaseAddress = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost"
+    val databaseAddress = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost/"
     val databaseDriver = System.getenv("DB_DRIVER") ?: "org.postgresql.Driver"
     val databaseUser = System.getenv("DB_USER") ?: ""
     val databasePassword = System.getenv("DB_PASSWORD") ?: ""
