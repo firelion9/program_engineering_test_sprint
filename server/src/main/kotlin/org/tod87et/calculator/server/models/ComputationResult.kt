6053a7cd06b703f06bc85ad8d5a5ff68ee712ff9
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-16 20:15:39 +0300

    Добавил еще одну модель и конвектор из FormulaEntry в ComputationResult

--- server/src/main/kotlin/org/tod87et/calculator/server/models/ComputationResult.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/models/ComputationResult.kt
@@ -2,7 +2,18 @@ package org.tod87et.calculator.server.models
 
 import kotlinx.serialization.Serializable
 import kotlinx.datetime.Instant
+import kotlinx.datetime.toKotlinInstant
+import org.tod87et.calculator.server.database.FormulaEntry
 
 // This class will be used for communication between server and client
 @Serializable
 data class ComputationResult(val id: String, val expression: String, val result: Double, val timestamp: Instant)
+
+fun FormulaEntry.toComputationResult(): ComputationResult {
+    return ComputationResult(
+        id = this.id.toString(),
+        expression = this.formula,
+        result = this.result,
+        timestamp = this.date.toKotlinInstant()
+    )
+}
