54e817047dfc87d3276ab7028622259637f25c81
Makar Selivanov <makar.selivanov@gmail.com>
2023-09-17 19:38:49 +0300

    change tests a little, only 2 test check for parser

--- server/src/test/kotlin/org/tod87et/calculator/server/ServerTest.kt
+++ server/src/test/kotlin/org/tod87et/calculator/server/ServerTest.kt
@@ -9,6 +9,7 @@ import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.assertAll
 import org.tod87et.calculator.server.database.FormulasDb
 import org.tod87et.calculator.server.models.ComputationResult
 import org.tod87et.calculator.server.models.ComputeRequest
@@ -26,126 +27,143 @@ class ServerTest {
     private val computePath = "$calculatorPath/compute"
 
     @Test
-    fun checkHistoryRoutes() {
-        testApplication {
-            var response = client.get(listPath)
-            assertEquals(HttpStatusCode.OK, response.status)
-            response = client.delete(removePath)
-            assertEquals(HttpStatusCode.BadRequest, response.status)
-        }
+    fun checkHistoryRoutes() = testApplication {
+        var response = client.get(listPath)
+        assertEquals(HttpStatusCode.OK, response.status)
+        response = client.delete(removePath)
+        assertEquals(HttpStatusCode.BadRequest, response.status)
     }
 
     @Test
-    fun checkCalculator_200_Ok() {
-        testApplication {
-            val expression = "1+2"
+    fun checkCalculator_200_Ok() = testApplication {
+        val expression = "1+2"
+        val response = client.post(computePath) {
+            setBody(ComputeRequest(expression))
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val body = response.body<ComputationResult>()
+        //assertEquals(3.0, body.result, "Calculator computes incorrect")
+        assertEquals(expression, body.expression, "Doesn't return correct expression")
+        val entry = database.selectAllFormulas().find { it.id == body.id.toInt() }
+        assertNotNull(entry, "Didn't find entry with ${body.id}")
+        assertEquals(entry.toComputationResult(), body, "Entry is not the same by id=${body.id}")
+    }
+
+    @Test
+    fun checkHistoryGet() = testApplication {
+        val requests = arrayOf("1 + 2" to 3, "6/2*(1+2)" to 9, "5^-1" to 0.2)
+        val buffer = mutableListOf<ComputationResult>()
+        requests.forEach { element ->
+            val expression = element.first
             val response = client.post(computePath) {
-                setBody(ComputeRequest(expression))
+                ComputeRequest(expression)
             }
             assertEquals(HttpStatusCode.OK, response.status)
+            buffer.add(response.body<ComputationResult>())
+        }
+        // ^ Preparations
+
+        //Check GET without query
+        var response = client.get(listPath)
+        var list = response.body<List<ComputationResult>>()
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContentEquals(buffer, list)
+
+        //Check GET with limit
+        response = client.get(listPath) {
+            url {
+                parameters.append("limit", "2")
+            }
+        }
+        list = response.body<List<ComputationResult>>()
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContentEquals(buffer.subList(1, 3), list)
+
+        //Check GET with offset
+        response = client.get(listPath) {
+            url {
+                parameters.append("offset", "1")
+            }
+        }
+        list = response.body<List<ComputationResult>>()
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContentEquals(buffer.subList(0, 2), list)
+
+        //Check GET with everything
+        response = client.get(listPath) {
+            url {
+                parameters.append("limit", "1")
+                parameters.append("offset", "1")
+            }
+        }
+        list = response.body<List<ComputationResult>>()
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContentEquals(buffer.subList(1, 2), list)
+    }
+
+    @Test
+    fun checkHistoryDelete() = testApplication {
+        val requests = arrayOf("5" to 5, "((10 + 2) / (6))" to 2, "10^0" to 1)
+        val buffer = mutableListOf<ComputationResult>()
+        requests.forEach { element ->
+            val expression = element.first
+            val response = client.post(computePath) {
+                ComputeRequest(expression)
+            }
+            assertEquals(HttpStatusCode.OK, response.status)
+            buffer.add(response.body<ComputationResult>())
+        }
+        // ^ Preparations
+
+        val order = intArrayOf(1, 0, 2)
+        order.forEach { index ->
+            val response = client.delete("$removePath/${buffer[index].id}")
             val body = response.body<ComputationResult>()
-            assertEquals(3.0, body.result, "Calculator computes incorrect")
-            assertEquals(expression, body.expression, "Doesn't return correct expression")
-            val entry = database.selectAllFormulas().find { it.id == body.id.toInt() }
-            assertNotNull(entry, "Didn't find entry with ${body.id}")
-            assertEquals(entry.toComputationResult(), body, "Entry is not the same by id=${body.id}")
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(buffer[index], body)
         }
+        val response = client.get(listPath)
+        val list = response.body<List<ComputationResult>>()
+        assertTrue(list.isEmpty())
     }
 
     @Test
-    fun checkCalculator_400_Bad_request() {
-        testApplication {
-            val expression = "*5"
+    fun checkParserResult() = testApplication {
+        val requests = arrayOf(
+            "1 + 2" to 3, "6/2*(1+2)" to 9, "5^-1" to 0.2,
+            "5" to 5, "((10 + 2) / (6))" to 2, "10^0" to 1,
+        )
+        val buffer = mutableListOf<ComputationResult>()
+        requests.forEach { element ->
+            val expression = element.first
             val response = client.post(computePath) {
-                setBody(ComputeRequest(expression))
+                ComputeRequest(expression)
             }
-            assertEquals(HttpStatusCode.BadRequest, response.status)
-            val body = response.bodyAsText()
-            //TODO add check for parse error message
-            assertEquals("kjsdfk", body, "Should be equal to parse error message, TODO add this check")
+            assertEquals(HttpStatusCode.OK, response.status)
+            buffer.add(response.body<ComputationResult>())
+        }
+        for (index in requests.indices) {
+            assertEquals(requests[index].second, buffer[index].result,
+                "Parser is not working for ${requests[index].first}")
         }
     }
 
     @Test
-    fun checkHistoryGet() {
-        testApplication {
-            val requests = arrayOf("1 + 2" to 3, "6/2*(1+2)" to 9, "5^-1" to 0.2)
-            val buffer = mutableListOf<ComputationResult>()
-            requests.forEach { element ->
-                val expression = element.first
-                val response = client.post(computePath) {
-                    ComputeRequest(expression)
-                }
-                assertEquals(HttpStatusCode.OK, response.status)
-                buffer.add(response.body<ComputationResult>())
-            }
-            // ^ Preparations
-
-            //Check GET without query
-            var response = client.get(listPath)
-            var list = response.body<List<ComputationResult>>()
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertContentEquals(buffer, list)
-
-            //Check GET with limit
-            response = client.get(listPath) {
-                url {
-                    parameters.append("limit", "2")
-                }
-            }
-            list = response.body<List<ComputationResult>>()
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertContentEquals(buffer.subList(1, 3), list)
-
-            //Check GET with offset
-            response = client.get(listPath) {
-                url {
-                    parameters.append("offset", "1")
-                }
-            }
-            list = response.body<List<ComputationResult>>()
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertContentEquals(buffer.subList(0, 2), list)
-
-            //Check GET with everything
-            response = client.get(listPath) {
-                url {
-                    parameters.append("limit", "1")
-                    parameters.append("offset", "1")
-                }
-            }
-            list = response.body<List<ComputationResult>>()
-            assertEquals(HttpStatusCode.OK, response.status)
-            assertContentEquals(buffer.subList(1, 2), list)
+    fun checkParserBadResult() = testApplication {
+        val expression = "*5"
+        val response = client.post(computePath) {
+            setBody(ComputeRequest(expression))
         }
-    }
-
-    @Test
-    fun checkHistoryDelete() {
-        testApplication {
-            val requests = arrayOf("5" to 5, "((10 + 2) / (6))" to 2, "10^0" to 1)
-            val buffer = mutableListOf<ComputationResult>()
-            requests.forEach { element ->
-                val expression = element.first
-                val response = client.post(computePath) {
-                    ComputeRequest(expression)
-                }
-                assertEquals(HttpStatusCode.OK, response.status)
-                buffer.add(response.body<ComputationResult>())
-            }
-            // ^ Preparations
-
-            val order = intArrayOf(1, 0, 2)
-            order.forEach { index ->
-                val response = client.delete("$removePath/${buffer[index].id}")
-                val body = response.body<ComputationResult>()
-                assertEquals(HttpStatusCode.OK, response.status)
-                assertEquals(buffer[index], body)
-            }
-            val response = client.get(listPath)
-            val list = response.body<List<ComputationResult>>()
-            assertTrue(list.isEmpty())
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+        var message = ""
+        try {
+            eval(expression)
+        } catch (e: Exception) {
+            message = e.message ?: ""
         }
+
+        val body = response.bodyAsText()
+        assertEquals(message, body, "Should be equal to parse error message")
     }
 
     @AfterEach
