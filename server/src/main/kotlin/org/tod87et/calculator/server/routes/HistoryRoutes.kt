5eb31bf2b9cd559056dd6667c217ca710f9e0311
DungeonMaster <st106332@student.spbu.ru>
2023-09-17 18:27:37 +0300

    history list query

--- server/src/main/kotlin/org/tod87et/calculator/server/routes/HistoryRoutes.kt
+++ server/src/main/kotlin/org/tod87et/calculator/server/routes/HistoryRoutes.kt
@@ -5,15 +5,37 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.builtins.ArraySerializer
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToJsonElement
 import org.tod87et.calculator.server.database
+import org.tod87et.calculator.server.models.ComputationResult
+import org.tod87et.calculator.server.models.toComputationResult
 
 fun Route.historyRouting() {
     route("/history") {
         route("/list") {
             get() {
-                val limit = call.request.queryParameters["limit"]?.toIntOrNull()
-                val offset = call.request.queryParameters["offset"]?.toIntOrNull()
-                //TODO
+                val limit = call.request.queryParameters["limit"]?.toIntOrNull() ?: Int.MAX_VALUE
+                val offset = call.request.queryParameters["offset"]?.toIntOrNull() ?: 0
+
+                try {
+                    val dbResponse = database.selectFormulas(limit, offset.toLong())
+                    val result = dbResponse.map {it.toComputationResult()}
+                    val jsonResult = Json.encodeToString(ArraySerializer<ComputationResult, ComputationResult>
+                        (ComputationResult.serializer()), result.toTypedArray())
+                    return@get call.respondText(
+                        jsonResult,
+                        status = HttpStatusCode.OK
+                    )
+                } catch (e: Exception) {
+                    return@get call.respondText(
+                        e.message ?: "Undefined server error",
+                        status = HttpStatusCode.InternalServerError
+                    )
+                }
+
+
             }
         }
         route("/remove") {
